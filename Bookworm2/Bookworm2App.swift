86aa061f6698486314fade3227f4f62efba64031
midkids <83714759+midkids@users.noreply.github.com>
2026-08-10 17:09:58 -0400

    Initial Commit

---
+++ Bookworm2/Bookworm2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm2App.swift
+//  Bookworm2
+//
+//  Created by Myron Snelson on 8/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
